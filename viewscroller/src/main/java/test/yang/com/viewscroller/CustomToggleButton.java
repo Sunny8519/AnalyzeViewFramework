100d93d76c50ac38e86b0d34184211f18b485b2b
niyang <niyang@zhiguaniot.com>
2017-08-29 19:34:48 +0800

    jsdkfhidjid

--- viewscroller/src/main/java/test/yang/com/viewscroller/CustomToggleButton.java
+++ viewscroller/src/main/java/test/yang/com/viewscroller/CustomToggleButton.java
@@ -1,9 +1,14 @@
 package test.yang.com.viewscroller;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 /**
  * @author NiYang
@@ -12,20 +17,60 @@ import android.widget.LinearLayout;
  */
 
 public class CustomToggleButton extends LinearLayout {
+    private static final String TAG = CustomToggleButton.class.getSimpleName();
+    private String mLeftText = null;
+    private String mRightText = null;
+    private float mLeftTextSize = 0f;
+    private float mRightTextSize = 0f;
+    private Drawable mToggleButtonBackground = null;
+    private ToggleButton mToggleButton = null;
 
     public CustomToggleButton(Context context) {
         this(context, null);
     }
 
     public CustomToggleButton(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
+        init(context, attrs);
     }
 
-    public CustomToggleButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    private void init(Context context, AttributeSet attributeSet) {
+        setOrientation(HORIZONTAL);
+        setGravity(Gravity.CENTER_VERTICAL);
+        if (attributeSet != null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.CustomToggleButton);
+            this.mLeftText = typedArray.getString(R.styleable.CustomToggleButton_leftText);
+            this.mRightText = typedArray.getString(R.styleable.CustomToggleButton_rightText);
+            this.mLeftTextSize = typedArray.getDimension(R.styleable.CustomToggleButton_leftTextSize, 28f);
+            this.mRightTextSize = typedArray.getDimension(R.styleable.CustomToggleButton_rightTextSize, 28f);
+            this.mToggleButtonBackground = typedArray.getDrawable(R.styleable.CustomToggleButton_toggleButtonBackground);
+            typedArray.recycle();
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        post(new Runnable() {
+            @Override
+            public void run() {
+                initView();
+            }
+        });
     }
 
     private void initView() {
+        productTextView(this.mLeftText, this.mLeftTextSize);
+        this.mToggleButton = new ToggleButton(getContext());
+        this.mToggleButton.setBackgroundDrawable(this.mToggleButtonBackground);
+        addView(this.mToggleButton);
+        productTextView(this.mRightText, this.mRightTextSize);
+    }
 
+    private void productTextView(String text, float textSize) {
+        TextView textView = new TextView(getContext());
+        textView.setText(text);
+        textView.setTextSize(textSize);
+        addView(textView);
     }
 }
